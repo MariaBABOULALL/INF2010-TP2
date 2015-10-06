3dda741e5163b7e49c21d0d8d1a2171b6435ddc5
Maria Baboulall <mababc@l4712-26.info.polymtl.ca>
2015-10-06 14:59:22 -0400

    exercice 1 probleme 2 OK

--- PostfixSolverMain.java
+++ PostfixSolverMain.java
@@ -13,7 +13,37 @@ public class PostfixSolverMain
 		for(String token : s.split("\\s")) 
 		{
 			//A completer
-			
+                    double item = 0;
+                    double resultat = 0;
+                    // Si l element est un nombre, on le parse en double et on l insere dans la stack
+                    if(token.matches("-?\\d+(\\.\\d+)?"))
+                    {
+                        item = Double.parseDouble(token);
+                        stack.push(item);
+                    
+                    // Sinon on extrait les deux derniers elements et on cherche le type de l operation
+                    }else{
+                        double premier = stack.pop();
+                        double deuxieme = stack.pop();
+                        if(token.equals("+"))
+                        {
+                            resultat = deuxieme + premier;
+                        }
+                        else if(token.equals("-"))
+                        {
+                            resultat = deuxieme - premier;  
+                        }
+                        else if(token.equals("*"))
+                        {
+                            resultat = deuxieme * premier; 
+                        }
+                        else if(token.equals("/"))
+                        {
+                            resultat = deuxieme / premier;
+                        }
+                        // On insere le resultat dans la stack
+                        stack.push(resultat);
+                    }
 		}
      
 		System.out.println("25 + 5*2 + 15/3 - 5 = "+stack.peek());
@@ -21,5 +51,6 @@ public class PostfixSolverMain
 			System.out.println("It's all good");
 		else
 			System.out.println("Erreur: mauvais resultat");
+                
      }
 }
